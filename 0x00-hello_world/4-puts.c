b13910a90a0c6f539c96bc5393d222531e530fbc
Ebtisam <sam0402@gmail.com>
2023-02-16 12:10:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <studio.h>
 /**
  * main - main block
- *return: 0
+ *Return: 0
  */
 int main(void)
 {
-  puts("\"Programming is like develop a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
