bc536c033a5c04b4406d8a4f7048928346863d5c
root <sam0402@gmail.com>
2023-03-02 13:38:04 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 
 
+#ifndef _main_h_
+#define _main_h_
 
-#ifndef main
-
-int_putchar(char c);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,7 +11,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
-#define main
 #endif
-char *cap_string(char *);
