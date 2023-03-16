40ff507074e13ec8cdde57fa7d74ca78c555f354
root <sam0402@gmail.com>
2023-03-16 15:55:11 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - multiplies two positive numbers
  * @argc: n arguments
@@ -23,7 +24,7 @@ int i, j;
 		}
 
 	}
-	mul = atol(argv[1]) *atol(argv[2]);
+	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
 return (0);
 }
